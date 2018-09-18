59f2559b99822b404e34734c4dce69a525116b0e
GioApaza <you@example.com>
2018-09-18 05:22:35 -0400

    EntidadesCtrl

--- src/control/ProveedorCtrl.java
+++ src/control/ProveedorCtrl.java
@@ -1,5 +1,118 @@
 package control;
 
-public class ProveedorCtrl {
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import entity.Proveedor;
+
+public class ProveedorCtrl implements Control<Proveedor>{  // implementa los metodos de control para cliente a interfase insert search delete update
+		
+		private Conexion conexion; // unico atributo
+		
+		public ProveedorCtrl (Conexion conexion) { //constructor de la clase conexion se ocupa de conectar java con la base de datos y es una interfase de datos 
+			this.conexion = conexion;             
+		}
+
+		/*
+		 * *****************************************************************************
+		 * List
+		 ******************************************************************************/
+		public ArrayList<Proveedor> list() throws Throwable { //arraylist es una estructura de tipo arreglo con tamaño indefinido es una estructura dinamica
+			ArrayList<Proveedor> proveedores = new ArrayList<Proveedor>();//devolver una lista cliente
+			ResultSet rs; //donde se guardan los resultados de nuestra consulta
+			int codigoProveedor;
+			String nombre;
+			String direccion;
+			int telefono;
+
+			conexion.SQL("Select * from proveedor"); // metemos la consulta
+
+			rs = conexion.resultSet(); // guarda el resultado de la consulta y devuelve todo lo de la consulta
+
+			while (rs.next()) { // while para que vaya recoriendo y nos devuelva todos los elementos del rs 
+				codigoProveedor = rs.getInt("codigoProveedor");
+				nombre = rs.getString("nombre");
+				direccion = rs.getString("direccion");
+				telefono = rs.getInt("telefono");
+				proveedores.add(new Proveedor(codigoProveedor, nombre, telefono, direccion)); // añade cliente nuevo 
+			}
+
+			return proveedores;
+
+		}
+
+		/*
+		 * *****************************************************************************
+		 * Insert
+		 ******************************************************************************/
+		public void insert(Proveedor proveedor) throws Throwable {
+System.out.println("Entro metodo");
+			conexion.SQL("Insert into proveedor(codigoProveedor,nombre, telefono,direccion) VALUES(?,?,?,?)");//?? son parametros esta esperando dos parametros
+			conexion.preparedStatement().setInt(1, proveedor.getCodigoProveedor()); // set tipo de datos
+			conexion.preparedStatement().setString(2, proveedor.getNombre());
+			conexion.preparedStatement().setString(4, proveedor.getDireccion());
+			conexion.preparedStatement().setInt(3, proveedor.getTelefono());
+			conexion.CUD();
+
+		}
+
+		/*
+		 * *****************************************************************************
+		 * Search
+		 ******************************************************************************/
+
+		public Proveedor search1(Proveedor proveedor) throws Throwable { // recibe el cliente y va buscar la clave del cliente y devulve el nombre a ese cliente y lo cierra
+			Proveedor prov= null;
+			ResultSet rs;
+
+			conexion.SQL("Select * from proveedor where codigoProveedor=?");
+			conexion.preparedStatement().setInt(1, proveedor.getCodigoProveedor());
+			rs = conexion.resultSet();
+
+			while (rs.next()) {
+				System.out.println("Encontro");
+				prov = new Proveedor(proveedor.getCodigoProveedor(),rs.getString("nombre"),rs.getInt("telefono"),rs.getString("direccion"));
+			}
+
+			rs.close();
+return prov;
+		}
+
+		/*
+		 * *****************************************************************************
+		 * Update
+		 ******************************************************************************/
+
+		public void update1(Proveedor proveedor) throws Throwable { // modifica datos del cliente con el nit
+			int codigoProveedor; //varchar entonces string
+			String nombre;
+			String direccion;
+			int telefono;
+			if (proveedor != null) {
+				codigoProveedor = proveedor.getCodigoProveedor();
+				nombre = proveedor.getNombre();
+				direccion = proveedor.getDireccion();
+				telefono = proveedor.getTelefono();
+
+				conexion.SQL("Update proveedor set nombre = ?,direccion = ?,telefono= ? where codigoProveedor=?");
+				conexion.preparedStatement().setString(1, nombre);
+				conexion.preparedStatement().setString(2, direccion);
+				conexion.preparedStatement().setInt(3, telefono);
+				conexion.preparedStatement().setInt(4, codigoProveedor);
+				conexion.CUD();
+			}
+		}
+
+		@Override
+		public void update(Proveedor proveedor) throws Throwable {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void search(Proveedor entity) throws Throwable {
+			// TODO Auto-generated method stub
+			
+		}
 }
+
