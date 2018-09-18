a469d03eb886dbd8b5d7dab3a4aa6d9bac6c6c8b
GioApaza <you@example.com>
2018-09-18 09:57:49 -0400

    Menu compra

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,5 +1,12 @@
 package aplicacion;
 
 public class Main {
-
+	public class Main extends Application {
+		@Override
+		public void start(Stage primaryStage) {
+			try {
+				Parent root = FXMLLoader.load(getClass().getResource("/application/menuPrincipal.fxml"));
+				Scene scene = new Scene(root,200,200);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(
 }
--- src/aplicacion/Menu_Compra_Actualizar_Proveedor.java
+++ src/aplicacion/Menu_Compra_Actualizar_Proveedor.java
@@ -1,5 +1,14 @@
 package aplicacion;
 
 public class Menu_Compra_Actualizar_Proveedor {
+	
+	Conexion conexion;
+	public void actualizar() throws Throwable {
+		conexion = new Conexion();
+		try {
+			int codigo = Integer.parseInt(TF1.getText());
+			String nombre = TF2.getText();
+			int telefono = Integer.parseInt(TF3.getText());
+			String direccion = TF4.getText();
 
 }
