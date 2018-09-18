0b18eddeb38f69d7f29f243d2f81cfa0d5b53711
GioApaza <you@example.com>
2018-09-18 15:44:54 -0400

    Menu Buscar Proveedor

---
+++ src/aplicacion/Menu_Compra_Buscar_Proveedor.java
@@ -0,0 +1,16 @@
+package aplicacion;
+
+public class Menu_Compra_Buscar_Proveedor {
+
+	
+	Conexion conexion;
+	public void buscar() throws Throwable {
+		conexion = new Conexion();
+		try {
+			int codigo = Integer.parseInt(TF1.getText());
+			
+			Proveedor proveedor = new Proveedor(codigo,"",0,"");
+			ProveedorCtrl proveedorCtrl = new ProveedorCtrl(conexion);
+			Proveedor proveedor1 =proveedorCtrl.search1(proveedor);
+			TF1.setTex
+}
