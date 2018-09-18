5e85eaa6fd7f4cc3a2476cd7620c5b01b1ec6746
GioApaza <42975671+GioApaza@users.noreply.github.com>
2018-09-18 14:09:48 -0400

    Update Main.java

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -8,5 +8,4 @@ public class Main {
 				Parent root = FXMLLoader.load(getClass().getResource("/application/menuPrincipal.fxml"));
 				Scene scene = new Scene(root,200,200);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			get.prime();
-}
+				
