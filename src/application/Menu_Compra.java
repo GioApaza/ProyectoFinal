311e282405f3fc90adf3a45522ec13fd18999bb9
GioApaza <you@example.com>
2018-09-19 04:38:54 -0400

    correction MenuCompra yMenu Venta

--- src/application/Menu_Compra.java
+++ src/application/Menu_Compra.java
@@ -1,8 +1,23 @@
 package application;
 
-public class Menu_Compra {
+import java.io.IOException;
+import java.util.ArrayList;
 
-Conexion conexion;
+import control.ClienteCtrl;
+import control.Conexion;
+import control.ProveedorCtrl;
+import entity.Cliente;
+import entity.Proveedor;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Menu_Compra {
+	@FXML private Label TXT;
+	Conexion conexion;
 	
 	public void opcionIngresarCompra() throws IOException {
 		Stage primaryStage = new Stage();
@@ -23,4 +38,41 @@ Conexion conexion;
 		primaryStage.show();
 		
 	}
+	public void listarproveedores() throws Throwable {
+		conexion = new Conexion();
+		ProveedorCtrl proveedorCtrl = new ProveedorCtrl(conexion);
+		ArrayList<Proveedor> listaProveedores = proveedorCtrl.list();
+		String datos = "";
+		for(Proveedor item : listaProveedores) {
+			datos += item.toString()+"\n";
+		}
+		TXT.setText(datos);
+		
+		
+	}
+	public void actualizar() throws IOException {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/MenuCompraActualizarProveedor.fxml"));
+
+		Scene scene = new Scene(root,450,450);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	public void insertarProveedor() throws IOException {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/menuCompraInsertarProveedor.fxml"));
+
+		Scene scene = new Scene(root,450,450);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+
+	}
+	public void salir() {
+		System.exit(0);
+	}
+	
 }
--- src/application/Menu_Compra_Insertar_Proveedor.java
+++ src/application/Menu_Compra_Insertar_Proveedor.java
@@ -1,8 +1,21 @@
 package application;
 
-public class Menu_Compra_Insertar_Proveedor {
+import control.ClienteCtrl;
+import control.Conexion;
+import control.ProveedorCtrl;
+import entity.Cliente;
+import entity.Proveedor;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-Conexion conexion;
+public class Menu_Compra_Insertar_Proveedor {
+	@FXML private TextField TF1;
+	@FXML private TextField TF2;
+	@FXML private TextField TF3;
+	@FXML private TextField TF4;
+	@FXML private Button BT;
+	Conexion conexion;
 	
 	public void guardar() throws Throwable {
 		conexion = new Conexion();
@@ -11,8 +24,15 @@ Conexion conexion;
 			String nombre = TF2.getText();
 			int telefono = Integer.parseInt(TF3.getText());
 			String direccion = TF4.getText();
-			Prov proveedor = new Proveedor(codigo,nombre,telefono,direccion);
+			Proveedor proveedor = new Proveedor(codigo,nombre,telefono,direccion);
 			ProveedorCtrl proveedorCtrl = new ProveedorCtrl(conexion);
 			proveedorCtrl.insert(proveedor);
 			
+			
+		}catch(Exception e) {
+			TF1.setText("Ingrese Valor Valido");
+			TF2.setText("Ingrese Valor Valido");
+		}
+		
+	}
 }
