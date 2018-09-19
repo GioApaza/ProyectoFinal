d17352c57a3a77e6c95056c08aba0c5b3bdc9424
GioApaza <you@example.com>
2018-09-19 04:33:10 -0400

    Avance java fx

--- src/application/Menu_Venta.java
+++ src/application/Menu_Venta.java
@@ -1,7 +1,23 @@
 package application;
 
-public class Menu_Venta {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import control.ClienteCtrl;
+import control.Conexion;
+import entity.Cliente;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+import javafx.stage.Stage;
+
+public class Menu_Venta {
+	@FXML private Label TXT1;
+	Conexion conexion;
+	
 	public void opcionIngresarVenta() throws IOException {
 		Stage primaryStage = new Stage();
 		Parent root = FXMLLoader.load(getClass().getResource("/application/menuVentaRealizarVenta.fxml"));
@@ -10,4 +26,48 @@ public class Menu_Venta {
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	public void busquedaCliente() throws IOException {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/menuVentaBuscarCliente.fxml"));
+
+		Scene scene = new Scene(root,500,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	public void listarclientes() throws Throwable {
+		conexion = new Conexion();
+		ClienteCtrl clienteCtrl = new ClienteCtrl(conexion);
+		ArrayList<Cliente> listaClientes = clienteCtrl.list();
+		String datos = "";
+		for(Cliente item : listaClientes) {
+			datos += item.toString()+"\n";
+		}
+		TXT1.setText(datos);
+		
+		
+	}
+	public void actualizarCliente() throws IOException {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/menuVentaActualizarCliente.fxml"));
+
+		Scene scene = new Scene(root,500,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	public void insertarCliente() throws IOException {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/menuVentaInsertarCliente.fxml"));
+
+		Scene scene = new Scene(root,500,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+
+	}
 }
